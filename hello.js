f2b4a5b1ee1b1398c9fa3c53aebb740c5734a2e7
ANJALI  ANIL  GHUTKE <114029880+anjalig2309@users.noreply.github.com>
2025-03-26 22:47:20 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,14 +1,18 @@
-
 fetch("hello.wasm")
-    .then(response => {
-        console.log(response);
-        return response.arrayBuffer();
-    })
+    .then(response => response.arrayBuffer())
     .then(bytes => WebAssembly.instantiate(bytes, {}))
     .then(results => {
         let instance = results.instance;
-        document.getElementById("output").innerText = "WebAssembly Loaded Successfully!";
-        instance.exports._main();
+
+        // Debugging: Check available exports
+        console.log("Exported functions:", instance.exports);
+
+        if (instance.exports._main) {
+            instance.exports._main();
+            document.getElementById("output").innerText = "WebAssembly Loaded Successfully!";
+        } else {
+            document.getElementById("output").innerText = "WebAssembly Loaded, but _main not found!";
+        }
     })
     .catch(error => {
         document.getElementById("output").innerText = "Error loading WebAssembly!";
