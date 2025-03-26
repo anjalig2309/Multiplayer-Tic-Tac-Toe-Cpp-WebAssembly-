151b1933406ef17ece93751b5b652142a9a09ae7
ANJALI  ANIL  GHUTKE <114029880+anjalig2309@users.noreply.github.com>
2025-03-26 22:31:38 +0530

    Update auth.cpp

--- auth.cpp
+++ auth.cpp
@@ -1,23 +1,23 @@
 #include <emscripten/bind.h>
 #include <string>
+#include <unordered_map>
 
 using namespace emscripten;
 
-std::string storedUsername = "player";
-std::string storedPassword = "1234";
+// User database with two players
+std::unordered_map<std::string, std::string> users = {
+    {"player1", "password1"},
+    {"player2", "password2"}
+};
 
 bool validateUser(std::string username, std::string password) {
-    return (username == storedUsername && password == storedPassword);
-}
-
-EMSCRIPTEN_BINDINGS(my_module) {
-    function("validateUser", &validateUser);
-}
-
-#include <iostream>
-
-extern "C" {
-    void _main() {
-        std::cout << "Hello from WebAssembly!" << std::endl;
+    auto it = users.find(username);
+    if (it != users.end() && it->second == password) {
+        return true;
     }
+    return false;
+}
+
+EMSCRIPTEN_BINDINGS(auth_module) {
+    function("validateUser", &validateUser);
 }
